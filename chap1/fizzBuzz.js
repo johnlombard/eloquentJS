3770b88e96f3a0ce06de1f2e73daf3ba1a1a0969
John Lombard <John@Johns-MBP.fios-router.home>
2018-11-02 23:30:25 -0400

    reviewing

---
+++ chap1/fizzBuzz.js
@@ -0,0 +1,12 @@
+for (i = 0; i <= 100; i++) {
+    if (i % 5 == 0 && i % 3 == 0) {
+        console.log(i, "fizzbuzz")
+    } else if (i % 3 == 0) {
+        console.log(i, "fizz")
+    } else if (i % 5 == 0) {
+        console.log(i, "buzz")
+    } else {
+        console.log(i);
+    }
+}
+
